cc1ce6d2c8f5493f9f4ec5f625263ffc3b63feee
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-06-05 12:22:24 -0500

    rearranging tickets passes tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -3,12 +3,26 @@ class Ticket:
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
-
+    def __str__(self):
+        return f'Ticket: {self.source}, {self.destination}'
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    #ticket(none, pdx)
+    #ticket(pdx, dca)
+    #ticket(dca, none)
+    
+    #starting location is none
+    #value = destination
+    
+    routeMap = {}
+    route = []
+    for ticket in tickets:
+        key = ticket.source
+        routeMap[key] = ticket.destination
+    
+    key = 'NONE'
+    while len(route) < length:
+        route.append(routeMap[key])
+        key = routeMap[key]
+    
     return route
